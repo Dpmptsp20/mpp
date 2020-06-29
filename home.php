eac6d76121a08530d1a549e76a6b050571fb10ec
Dpmptsp20 <66717200+Dpmptsp20@users.noreply.github.com>
2020-06-29 13:29:24 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,184 @@
+<div class=" process-v2-noborder" style="margin-top:-50px;">
+    <div class="col-sm-3">
+        <div class="process-v2">
+            <a href=""><span class="process-v2-element radius-circle"><i class="fa fa-book"></i></span></a>
+            <h3 class="process-v2-title">Penanaman Modal</h3>
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="process-v2">
+            <a href="<?php echo site_url('jenis-perizinan');?>"><span class="process-v2-element radius-circle"><i class="fa fa-bullhorn"></i></span></a>
+            <h3 class="process-v2-title">Jenis Perizinan</h3>
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="process-v2">
+            <a href="<?php echo site_url('search-perusahaan');?>"><span class="process-v2-element radius-circle"><i class="fa fa-search"></i></span></a>
+            <h3 class="process-v2-title">Cari Perusahaan</h3>
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="process-v2">
+            <a href="<?php echo site_url('search-perizinan');?>"><span class="process-v2-element radius-circle"><i class="fa fa-check-square-o"></i></span></a>
+            <h3 class="process-v2-title">Cek Perizinan</h3>
+        </div>
+    </div>
+</div>
+<div class="bg-color-sky">
+    <div class="content container">                    
+    </div>
+</div>
+<div class="bg-color-sky-light">
+    <div class="content container">  
+        <div class="divider-v1"><h2 class="heading-v4-title">Selamat Datang</h2></div>
+        <!-- End Divider v1 -->
+
+        <p><img class="wow flip animated" src="<?php echo base_url(); ?>assets/img/ptspnaker.png" align="left" width="200" height="200" style="visibility: visible;">Sebagai konsekuensi dari pelaksanaan otonomi daerah, Pemerintah Kabupaten Batang berupaya memberikan peningkatan pelayanan dan kesejahteraan yang semakin baik kepada masyarakat, tuntutan untuk memberikan pelayanan tersebut merupakan keharusan dan tidak dapat diabaikan dan ditunda lagi.</p>
+        <p>Dalam menyongsong era globalisasi dan akan segera diberlakukannya pasar bebas, tentunya akan terjadi kompetisi yang sangat ketat dalam bidang usaha maupun investasi yang selanjutnya akan banyak membutuhkan informasi peluang usaha, perijinan maupun dokumen lainnya.</p>
+        <p>Untuk mengatasi hal tersebut Pemerintah Kabupaten Batang berdasarkan Peraturan Daerah Nomor 4 Tahun 2008 tentang Pembentukan Susunan Organisasi dan Tata Kerja Lembaga Teknis Daerah dan Satuan Polisi Pamong Praja Kabupaten Batang, sebagaimana telah diubah dengan Peraturan Daerah Nomor 25 Tahun 2011 tentang Perubahan Atas Peraturan Daerah  Kabupaten Batang Nomor 4 Tahun 2008. Pemerintah Kabupaten Batang membentuk Badan Penanaman Modal dan Perijinan Terpadu Kabupaten Batang yang disingkat dengan BPMPT. Dengan terbentuknya BPMPT tersebut maka pelayanan kepada masyarakat di Kabupaten Batang dibidang peijinan diharapkan dapat dilaksanakan dengan mudah, cepat, transparan, dan pasti.</p>
+        <p>Seiring dengan dibentuknya BPMPT Kabupaten Batang patut dikiranya disambut dengan rasa syukur kehadirat Tuhan YME semoga komitmen Pemerintah Kabupaten Batang dalam memberikan pelayanan prima kepada masyarakat dan dunia usah dapat lebih tertib, lancar sebagaimana yang menjadi harapan masyarakat.</p>
+    </div>
+</div>
+
+<!-- content-->
+
+<div class="bg-color-sky-light">
+    <div class="content-md container">
+        <!-- Heading v1 -->
+        <div class="heading-v1 text-center margin-b-90">
+            <h2 class="heading-v1-title">Galery Kegiatan DPMPTSP Kabupaten Batang</h2>
+            <p class="heading-v1-text">Let's invest in Batang Regency..</p>
+        </div>
+        <!-- End Heading v1 -->
+
+        <!-- News v7 -->
+        <div class="row">
+            <div class="col-sm-4 sm-margin-b-50">
+                <!-- News v7 -->
+                <article class="news-v7">
+                    <img class="img-responsive" src="<?php echo base_url(); ?>assets/img/pameran.png" alt="">
+                    <div class="news-v7-content text-center">
+                        <h2 class="news-v7-title"><a href="#">Central Java Investment Bussiness Forum </a></h2>
+                        <p class="news-v7-subtitle">Pameran Potensi dan Peluang Investasi 35 Kabupaten/Kota di Jawa Tengah</p>
+                    </div>
+                </article>
+                <!-- End News v7 -->
+            </div>
+            <div class="col-sm-4 sm-margin-b-30">
+                <!-- News v7 -->
+                <article class="news-v7 news-v7-lg">
+                    <div class="image-gallery-video">
+                        <a class="popup-vimeo" href="https://www.youtube.com/watch?v=rpXj4jf01J4" title="Perijinan satu pintu">
+                            <img class="img-responsive" src="<?php echo base_url(); ?>assets/img/kantorbpmpt.png" alt="">
+                            <span class="image-gallery-video-center-align">
+                                <img class="image-gallery-video-player" src="<?php echo base_url(); ?>assets/img/widgets/video-play.png" alt="">
+                                <span class="image-gallery-video-title">Lihat video</span>
+                            </span>
+                        </a>
+                    </div>
+                    <div class="news-v7-content text-center">
+                        <h2 class="news-v7-title"><a href="#">Profil BPMPT</a></h2>
+                        <p class="news-v7-subtitle">Video Profil Investasi &amp; Perijinan di Kabupaten Batang</p>
+                    </div>
+                </article>
+                <!-- End News v7 -->
+            </div>
+            <div class="col-sm-4">
+                <!-- News v7 -->
+                <article class="news-v7">
+                    <img class="img-responsive" src="<?php echo base_url(); ?>assets/img/sosialisasi2017.jpg" alt="">
+                    <div class="news-v7-content text-center">
+                        <h2 class="news-v7-title"><a href="#">Sosialisasi</a></h2>
+                        <p class="news-v7-subtitle">Sosialisasi Pelayanan Administrasi Terpadu Kecamatan (PATEN)</p>
+                    </div>
+                </article>
+                <!-- End News v7 -->
+            </div>
+            <div class="col-sm-4 sm-margin-b-30">
+                <!-- News v7 -->
+                <article class="news-v7 news-v7-lg">
+                    <div class="image-gallery-video">
+                        <a class="popup-vimeo" href="https://www.youtube.com/watch?v=dgVNZpEhawg" title="Perijinan satu pintu">
+                            <img class="img-responsive" src="<?php echo base_url(); ?>assets/img/mpp.jpg" alt="">
+                            <span class="image-gallery-video-center-align">
+                                <img class="image-gallery-video-player" src="<?php echo base_url(); ?>assets/img/widgets/video-play.png" alt="">
+                                <span class="image-gallery-video-title">Lihat video</span>
+                            </span>
+                        </a>
+                    </div>
+                    <div class="news-v7-content text-center">
+                        <h2 class="news-v7-title"><a href="#">LOMBA INOVASI</a></h2>
+                        <p class="news-v7-subtitle">LAYANAN BLEDEK &amp; MPP Kabupaten Batang</p>
+                    </div>
+                </article>
+                <!-- End News v7 -->
+            </div>
+        </div>
+        <!--// end row -->
+        <!-- End News v7 -->
+
+    </div>
+
+</div>
+<div class="heading-v1 text-center margin-b-80">
+    <h2 class="heading-v1-title">Link</h2>
+    <p class="heading-v1-text">Aplikasi Pelayanan Umum Pemerintahan Kabupaten Batang.</p>
+</div>
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <!-- Owl Carousel Clients Five Item -->
+       <ul class="list-inline owl-carousel-clients-six-item clients-v1">
+            <li class="item">
+                <div class="clients-v1-item">
+                    <a href="http://portal.batangkab.go.id/spgdt" target="_blank">
+                        <img class="clients-v1-img clients-v1-img-default" src="<?php echo base_url(); ?>assets/img/link/911.png" alt="">
+                        <img class="clients-v1-img clients-v1-img-active" src="<?php echo base_url(); ?>assets/img/link/911grain.png" alt="">
+                    </a>
+                </div>
+            </li>
+            <li class="item">
+                <div class="clients-v1-item">
+                    <a href="https://lpse.batangkab.go.id/" target="_blank">
+                        <img class="clients-v1-img clients-v1-img-default" src="<?php echo base_url(); ?>assets/img/link/lpse.png" alt="">
+                        <img class="clients-v1-img clients-v1-img-active" src="<?php echo base_url(); ?>assets/img/link/lpsegrain.png" alt="">
+                    </a>
+                </div>
+            </li>
+            <li class="item">
+                <div class="clients-v1-item">
+                    <a href="http://ppid.batangkab.go.id/" target="_blank">
+                        <img class="clients-v1-img clients-v1-img-default" src="<?php echo base_url(); ?>assets/img/link/ppid.png" alt="">
+                        <img class="clients-v1-img clients-v1-img-active" src="<?php echo base_url(); ?>assets/img/link/ppidgrain.png" alt="">
+                    </a>
+                </div>
+            </li>
+            <li class="item">
+                <div class="clients-v1-item">
+                    <a href="http://jdih.batangkab.go.id/" target="_blank">
+                        <img class="clients-v1-img clients-v1-img-default" src="<?php echo base_url(); ?>assets/img/link/jdih.png" alt="">
+                        <img class="clients-v1-img clients-v1-img-active" src="<?php echo base_url(); ?>assets/img/link/jdihgrain.png" alt="">
+                    </a>
+                </div>
+            </li>
+            <li class="item">
+                <div class="clients-v1-item">
+                    <a href="http://upkp2.batangkab.go.id/" target="_blank">
+                        <img class="clients-v1-img clients-v1-img-default" src="<?php echo base_url(); ?>assets/img/link/upkp2.png" alt="">
+                        <img class="clients-v1-img clients-v1-img-active" src="<?php echo base_url(); ?>assets/img/link/upkp2grain.png" alt="">
+                    </a>
+                </div>
+            </li>
+            <li class="item">
+                <div class="clients-v1-item">
+                    <a href="http://portal.batangkab.go.id/iujk" target="_blank">
+                        <img class="clients-v1-img clients-v1-img-default" src="<?php echo base_url(); ?>assets/img/link/iujk.png" alt="">
+                        <img class="clients-v1-img clients-v1-img-active" src="<?php echo base_url(); ?>assets/img/link/iujkgrain.png" alt="">
+                    </a>
+                </div>
+            </li>
+           
+        </ul>
+        <br>
+        <!-- End Owl Carousel Clients Five Item -->
+    </div>
+</div>
